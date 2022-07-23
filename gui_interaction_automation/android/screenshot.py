b63f57bd8de1a17911058db9ffd6150e6f61bec9
Mee <rafaleto@gmail.com>
2022-07-23 11:35:20 +0200

    feat: Loaded a few missing things

--- gui_interaction_automation/android/screenshot.py
+++ gui_interaction_automation/android/screenshot.py
@@ -8,11 +8,34 @@ def screenshot(device:Device, filename:str) -> None:
         fp.write(result)
 
 
+
 if __name__ == "__main__":
 
+    import numpy as np
+    import PIL.Image as Image
+    import matplotlib
+    import matplotlib.pyplot as plt
+    from typing import * # type: ignore
     from gui_interaction_automation.android import connect_android
 
     with connect_android("RFCMB00509J") as device:
+
+        c = 0
         while True:
-            screenshot(device, r"C:\Users\Cherrypie\Desktop\s.png")
+
+            screenshot(device, rf"C:\Users\Cherrypie\Desktop\ugh_{c}.png")
+
+            result = device.screencap()
+            with open("tmpfile.png", "wb") as fp:
+                fp.write(result)
+            img    = Image.open("tmpfile.png")
+            img_np = np.array(img)
+            if img_np.shape[-1] == 4:
+                img_np = img_np[:,:,:3]
+
+            plt.imshow(img_np)
+            plt.show()
+
+            c += 1
+
             p = 0
